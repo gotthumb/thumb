fdda2c95ea7d8f661ff97473769b5f108f4e7abc
gotthumb <gotthumb@outlook.jp>
2024-08-30 18:37:30 +0900

    [Rn + Rm]L = Rd 命令を追加

--- m0plus/m0plus.pl
+++ m0plus/m0plus.pl
@@ -388,6 +388,16 @@
       $u5  = 0 ;
       printf "[R%d + R%d]W = R%d\n", $rgn, $rgm, $rgd ;
    }
+   # [Rn + Rm]L = Rd
+   elsif ( ( $opcode & 0xfe00 ) == 0x5000 )
+   {
+      $pc = $pc + 2;
+      $rgd = ( $opcode & 0x0007 ) ;
+      $rgn = ( $opcode & 0x0038 ) / 0x8 ;
+      $rgm = ( $opcode & 0x01c0 ) / 0x40 ;
+      $u5  = 0 ;
+      printf "[R%d + R%d]L = R%d\n", $rgn, $rgm, $rgd ;
+   }
    else
    {
       $pc = $pc + 2;
